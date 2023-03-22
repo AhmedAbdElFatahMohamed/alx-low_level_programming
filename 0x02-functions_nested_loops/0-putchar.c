b2d8521441637fe1355843638e67ee85e9cc6ce6
Ahmed Abd Elfatah <109298267+AhmedAbdElFatahMohamed@users.noreply.github.com>
2023-03-22 22:51:14 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,16 @@
-#include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
+#include "putchar.h"
+/**
+ * main - prints the string "Holberton" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char c[] = "putchar";
+	int i;
 
-
-	int main(void)
+	for (i = 0; i < 9; i++)
 	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+		_putchar(c[i]);
+	}
 	_putchar('\n');
 	return (0);
-	}
