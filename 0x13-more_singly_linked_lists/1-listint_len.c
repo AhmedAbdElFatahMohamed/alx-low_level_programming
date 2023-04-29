2fd3ed7a309ce0baef0c3c4ed9e36a73635ad6f7
Ahmed Abd Elfatah <109298267+AhmedAbdElFatahMohamed@users.noreply.github.com>
2023-04-29 13:24:51 +0200

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,20 @@
+#include "lists.h"
+
 /**
- * listint_len - returns the number of elements in a linked listint_t list
- * @h: pointer to the head of the linked list
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
  *
- * Return: number of elements in the linked list
+ * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
-    size_t count = 0;
+	size_t num = 0;
 
-    while (h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
+	while (h)
+	{
+		y++;
+		h = h->next;
+	}
 
-    return count;
+	return (y);
 }
